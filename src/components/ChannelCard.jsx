4c6159baf36830c26f87b0bcb88e225d60f7cd7a
thefidanabdulla <thefidanabdulla@gmail.com>
2022-10-06 13:49:43 +0400

    some updates

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import React from "react";
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
-  )
-}
+import { demoProfilePicture } from "../utils/constants";
 
-export default ChannelCard
+const ChannelCard = ({ channelDetail }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+    }}
+  >
+    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+      <CardContent
+        sx={{
+          color: "#FFF",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <CardMedia 
+          sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
+          image={ channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture }
+          alt={channelDetail?.snippet?.title}
+        />
+        <Typography variant="h6">
+          {channelDetail?.snippet?.title}
+        </Typography>
+      </CardContent>
+    </Link>
+  </Box>
+);
+
+export default ChannelCard;
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -11,7 +11,7 @@ const Feed = () => {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => {
       setVideos(data.items);
     });
-  }, []);
+  }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
